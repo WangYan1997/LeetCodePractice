05094ab5e83f7a6daefbf28127966828ccaad89f
ydfbhml <boxforwy@qq.com>
2019-06-21 18:29:29 +0800

    Valid Anagram / sort / Java / Version 4 / Optimization of version 1, use string.toCharArray()  instead of StringBuffer

--- Valid Anagram/src/Solution.java
+++ Valid Anagram/src/Solution.java
@@ -1,25 +1,81 @@
 class Solution {
-		public boolean isAnagram(String s, String t) {
-		
-        int Ls = s.length();
-        int Lt = t.length();
-		if(Ls != Lt) {
+	public boolean isAnagram(String s, String t) {
+		int lengthofs = s.length();
+		int lengthoft = t.length();
+
+		if (lengthofs != lengthoft) {
 			return false;
 		}
-		
-		int[] ch = new int[26];
-		
-		for(int i = 0; i < Ls; i++) {
-			ch[s.charAt(i) - 'a']++;
+
+		int left = 0;
+		int right = lengthofs - 1;
+
+		char[] $s = s.toCharArray();
+		char[] $t = t.toCharArray();
+		sort($s, left, right);
+		sort($t, left, right);
+
+		for (int i = 0; i < lengthofs; i++) {
+			if ($s[i] != $t[i]) {
+				return false;
+			}
 		}
-		
-		for(int i = 0; i < Lt; i++) {
-			ch[t.charAt(i) - 'a']--;
-            if(ch[t.charAt(i) - 'a'] < 0){
-                return false;
-            } 
-		}
-		
+
 		return true;
+	}
+
+	private char[] sort(char[] str, int l, int r) {
+
+		if (r <= l) {
+			return str;
+		}
+
+		int pivot = (l + r) / 2;
+
+		swap(str, pivot, r);
+		pivot = Partition(str, l, r);
+		sort(str, l, pivot - 1);
+		sort(str, pivot + 1, r);
+
+		return str;
+	}
+
+	private int Partition(char[] str, int left, int right) {
+		int l = left;
+		int r = right;
+
+		char temp = str[r];
+
+		while (l != r) {
+			while (str[l] <= temp && r > l) {
+				l++;
+			}
+			if (l < r) {
+				str[r] = str[l];
+				r--;
+			}
+
+			while (str[r]>= temp && r > l) {
+				r--;
+			}
+
+			if (l < r) {
+				str[l] = str[r];
+				l++;
+			}
+
+		}
+		str[l] = temp;
+		return l;
+	}
+
+	private char[] swap(char[] str, int i, int j) {
+		char temp;
+
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+
+		return str;
 	}
 }
