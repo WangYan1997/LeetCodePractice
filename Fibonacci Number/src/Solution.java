f4893b04509da21995768c514c7f367d00f032d1
ydfbhml <boxforwy@qq.com>
2019-08-11 18:33:20 +0800

    Fibonacci Number / Java / O(n)

--- Fibonacci Number/src/Solution.java
+++ Fibonacci Number/src/Solution.java
@@ -4,7 +4,7 @@
  * @version 1.0
  * @date 2019年8月11日 下午6:17:07
  * @remarks TODO
- * 斐波那契数列，使用递归的方式实现，时间复杂度为 O(2^n)
+ * 斐波那契数列，使用数组存储F(N - 1)和F(N - 2)，时间复杂度为 O(n)
  */
 public class Solution {
 	public int fib(int N) {
@@ -13,8 +13,16 @@ public class Solution {
 			return N;
 		}
 		
-		int f = fib(N - 1) + fib(N - 2);
+		int[] f = new int[N + 1];
 		
-		return f;
+		f[0] = 0;
+		f[1] = 1;
+		
+		for(int i = 2; i <= N; i++) {
+			f[i] = f[i - 1] + f[i - 2];
+		}
+		
+		return f[N];
+		
 	}
 }
