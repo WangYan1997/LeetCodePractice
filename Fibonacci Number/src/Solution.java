0db1c01675bcd27aa4961154725093257377d064
ydfbhml <boxforwy@qq.com>
2019-08-11 20:59:40 +0800

    Fibonacci Number / Java / the formula of general term

--- Fibonacci Number/src/Solution.java
+++ Fibonacci Number/src/Solution.java
@@ -4,24 +4,11 @@
  * @version 1.0
  * @date 2019年8月11日 下午6:17:07
  * @remarks TODO
- * 斐波那契数列，使用临时变量存储F(n-1)和F(n-2)，时间复杂度为 O(n)
+ * 斐波那契数列，使用公式计算
  */
 public class Solution {
 	public int fib(int N) {
-		int f = N;
-		
-		if(N <= 1) {
-			return f;
-		}
-		
-		int iter1 = 0;
-		int iter2 = 1;
-		
-		for(int i = 2; i <= N; i++) {
-			f = iter1 + iter2;
-			iter1 = iter2;
-			iter2 = f;
-		}
-		return f;
+		double f = ( Math.pow( (1 + Math.sqrt(5))/2 , N) - Math.pow( (1 - Math.sqrt(5))/2 , N)) / Math.sqrt(5);
+		return (int) f;
 	}
 }
