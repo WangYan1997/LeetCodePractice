61a9a2b9c20134267cac0c0142addfaff6c115ff
ydfbhml <boxforwy@qq.com>
2019-08-11 20:38:12 +0800

    Fibonacci Number / Java / iteration / O(n) / save memory

--- Fibonacci Number/src/Main.java
+++ Fibonacci Number/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int N = 5;
+		int N = 6;
 		
 		Solution s = new Solution();
 		int fib = s.fib(N);
--- Fibonacci Number/src/Solution.java
+++ Fibonacci Number/src/Solution.java
@@ -4,25 +4,24 @@
  * @version 1.0
  * @date 2019年8月11日 下午6:17:07
  * @remarks TODO
- * 斐波那契数列，使用数组存储F(N - 1)和F(N - 2)，时间复杂度为 O(n)
+ * 斐波那契数列，使用临时变量存储F(n-1)和F(n-2)，时间复杂度为 O(n)
  */
 public class Solution {
 	public int fib(int N) {
+		int f = N;
 		
 		if(N <= 1) {
-			return N;
+			return f;
 		}
 		
-		int[] f = new int[N + 1];
-		
-		f[0] = 0;
-		f[1] = 1;
+		int iter1 = 0;
+		int iter2 = 1;
 		
 		for(int i = 2; i <= N; i++) {
-			f[i] = f[i - 1] + f[i - 2];
+			f = iter1 + iter2;
+			iter1 = iter2;
+			iter2 = f;
 		}
-		
-		return f[N];
-		
+		return f;
 	}
 }
