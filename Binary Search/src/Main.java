35ac3fc98fee64e422214bf00a509f95a29bd9e9
ydfbhml <boxforwy@qq.com>
2019-08-12 22:17:47 +0800

    Binary Search / Java

---
+++ Binary Search/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int[] input = {-1,0,3,5,9,12};
+//		int target = 9;
+		
+		int[] input = {2,5};
+		int target = 5;
+		
+		Solution s = new Solution();
+		int index = s.search(input, target);
+		System.out.print(index);
+	}
+
+}
