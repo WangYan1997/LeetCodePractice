b1628fd091aaae198e70c735ef6427a1e0b64b7d
ydfbhml <boxforwy@qq.com>
2019-08-12 11:57:11 +0800

    Binary Tree Level Order Traversal / Java / 根据队列的长度，判断当前结点在哪一层

--- Binary Tree Level Order Traversal/src/Solution.java
+++ Binary Tree Level Order Traversal/src/Solution.java
@@ -1,39 +1,35 @@
 import java.util.*;
 
 public class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-    	TreeNode p = root;
-    	TreeNode last = root;
-    	TreeNode nextlast = root;
-    	List<List<Integer>> res = new ArrayList<List<Integer>>();
-    	List<Integer> level = new ArrayList<>();
-    	
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
-        
-        if(p != null) {
-        	q.add(p);
-        }
-        
-        while( !q.isEmpty()) {
-        	 p = q.poll();
-        	 level.add(p.val);
-        	 if(p.left != null) {
-        		 q.add(p.left);
-        		 nextlast = p.left;
-        	 }
-        	 if(p.right != null) {
-        		 q.add(p.right);
-        		 nextlast = p.right;
-        	 }
-        	 
-        	 if(p == last) {
-        		 res.add(level);
-        		 level = new ArrayList<>();
-        		 last = nextlast;
-        	 }
-        }
-        
-        return res;
-        
-    }
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		TreeNode p = root;
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		List<Integer> level = new ArrayList<>();
+
+		Queue<TreeNode> q = new LinkedList<TreeNode>();
+
+		if (p != null) {
+			q.add(p);
+		}
+
+		while (!q.isEmpty()) {
+
+			int size = q.size();
+			level = new ArrayList<>();
+			for (int i = 0; i < size; i++) {
+				p = q.poll();
+				level.add(p.val);
+				if (p.left != null) {
+					q.add(p.left);
+				}
+				if (p.right != null) {
+					q.add(p.right);
+				}
+			}
+			res.add(level);
+		}
+
+		return res;
+
+	}
 }
