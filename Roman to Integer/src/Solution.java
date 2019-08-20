837f6c95fabfce650e6b27a7843265705132a60f
ydfbhml <boxforwy@qq.com>
2019-08-20 20:29:55 +0800

    Roman to Integer / Java / String / very smart and elegant

--- Roman to Integer/src/Solution.java
+++ Roman to Integer/src/Solution.java
@@ -1,66 +1,33 @@
 
 public class Solution {
-	public int romanToInt(String s) {
-		int sum = 0, i = 0;
-		int len = s.length();
-
-		while (i < len) {
-			switch (s.charAt(i)) {
+	public static int romanToInt(String s) {
+		int res = 0;
+		for (int i = s.length() - 1; i >= 0; i--) {
+			char c = s.charAt(i);
+			switch (c) {
 			case 'I':
-				if (i < len - 1 && s.charAt(i + 1) == 'V') {
-					sum += 4;
-					i++;
-					break;
-				}
-				if (i < len - 1 && s.charAt(i + 1) == 'X') {
-					sum += 9;
-					i++;
-					break;
-				}
-				sum += 1;
+				res += (res >= 5 ? -1 : 1);
 				break;
 			case 'V':
-				sum += 5;
+				res += 5;
 				break;
 			case 'X':
-				if (i < len - 1 && s.charAt(i + 1) == 'L') {
-					sum += 40;
-					i++;
-					break;
-				}
-				if (i < len - 1 && s.charAt(i + 1) == 'C') {
-					sum += 90;
-					i++;
-					break;
-				}
-				sum += 10;
+				res += 10 * (res >= 50 ? -1 : 1);
 				break;
 			case 'L':
-				sum += 50;
+				res += 50;
 				break;
 			case 'C':
-				if (i < len - 1 && s.charAt(i + 1) == 'D') {
-					sum += 400;
-					i++;
-					break;
-				}
-				if (i < len - 1 && s.charAt(i + 1) == 'M') {
-					sum += 900;
-					i++;
-					break;
-				}
-				sum += 100;
+				res += 100 * (res >= 500 ? -1 : 1);
 				break;
 			case 'D':
-				sum += 500;
+				res += 500;
 				break;
 			case 'M':
-				sum += 1000;
+				res += 1000;
 				break;
 			}
-			i++;
 		}
-
-		return sum;
+		return res;
 	}
 }
