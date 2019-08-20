748ebe4a450a9e94689032848dddce8578326ba1
ydfbhml <boxforwy@qq.com>
2019-08-20 20:10:12 +0800

    Roman to Integer / Java / String

---
+++ Roman to Integer/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String input = new String("MCMXCIV");
+		
+		Solution s = new Solution();
+		int output = s.romanToInt(input);
+		System.out.print(output);
+	}
+
+}
